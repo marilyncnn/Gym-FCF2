ef67e367d4c472879ca2a6a36aa61b7c478a019c
marilyncnn <marilyncnn@gmail.com>
2018-06-19 14:02:28 -0700

    add index css

---
+++ applogic/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+    <head>
+        <title>Feature Code Forecast - Version 2.0</title>
+
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Biryani:200,300,400,600,700,800,900" rel="stylesheet">
+
+
+
+    </head>
+    <body>
+
+        <center>
+        <h3>Gymboree Group</h3>
+        <h1>Feature Code</h1>
+        <h2>Version 2.0</h2>
+        <div class="actions">
+
+            <a href="excel/FC reporting v2.xlsm" class = "action-link">
+                <div class = "action">
+
+                    <i class="material-icons">layers</i>
+                    <p>Reporting</p>
+                </div>
+            </a>
+            <a href="excel/FC Sales Forecast v2.xlsm" class = "action-link">
+                <div class = "action">
+
+                    <i class="material-icons">border_color</i>
+                    <p>Forecast Planning</p>
+                </div>
+            </a>
+            <a href="#" class = "action-link">
+            <div class = "action" >
+                <i class="material-icons">airport_shuttle</i>
+                <p>Allocation Planning</p>
+            </div>
+            <a href="applogic/view-seed.php" class = "action-link">
+            <div class = "action">
+                <i class="material-icons">settings</i>
+                <p>Feature Code Admin</p>
+            </div>
+        </a>
+        </center>
+    </body>
+</html>
